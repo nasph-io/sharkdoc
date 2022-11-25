321851e96e184c1e7d6bb1ac8cb437cf2d7f06f7
Junior <105942984+junioremerson@users.noreply.github.com>
2022-11-25 15:25:54 -0300

    Fix: removing timeout GitHub service

--- services/github.js
+++ services/github.js
@@ -4,7 +4,7 @@ import tar from "tar";
 
 const GitHubClient = axios.create({
   baseURL: "https://api.github.com/",
-  timeout: 1000,
+  // timeout: 1000,
   headers: {
     Accept: "application/vnd.GitHub.v3+json",
     //'Authorization': 'token <your-token-here> -- https://docs.github.com/pt/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
